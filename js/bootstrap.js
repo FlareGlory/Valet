b67ea59ecdabe624e15334e9840fffa70c67965a
hashimo326 <hashimo326@gmail.com>
2017-12-18 10:54:50 +0900

    カートに入れるを追加・削除

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2318,3 +2318,16 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+});
